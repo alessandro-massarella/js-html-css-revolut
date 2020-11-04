bb423c843913bb69e36d19db779fe905121c87d7
alessandro <alessandro.massarella@gmail.com>
2020-11-04 16:37:22 +0100

    js - prima prova

---
+++ script/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $('.nav_right .help').click(function() {
+    $('nav_right').children().removeClass('active');
+    $('.nav_right .pannello_help').addClass('active');
+  })
+
+
+
+
+
+})
